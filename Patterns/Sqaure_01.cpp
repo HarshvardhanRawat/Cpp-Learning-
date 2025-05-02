a5ce55fa7cc2d8cb258624a59c675a8a9b37ee76
H A R S H <fest.nov01@gmail.com>
2025-05-02 16:17:41 +0530

    Add files via upload

---
+++ Patterns/Sqaure_01.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n=3;
+
+    for(int i=0;i<n;i++){ //for int
+        for(int j=1;j<=n;j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
---
+++ Patterns/Square_03.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n=3;
+    int num=0;
+
+    for(int i=0; i<n ;i++){ //for counting
+        for (int j=0 ;j<n ;j ++){
+            
+        cout<<num<<" ";
+        num ++; 
+
+       }
+       cout<<endl;
+    }
+
+    return 0;
+}
